34c956390d3356f476330648d326ccea7303de6e
NekrasovaOlga <nekrasova.kalina@yandex.ru>
2022-08-25 15:03:28 +0300

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+const item = "Носки фиолетовые";
+const quantity = 10;
+const category = "Носки";
+const price = 100;
+
+console.log('Название товара:', item);
+console.log('Количество товара:', price * quantity, 'руб.' );
+
