533038961b748e5c785fe0571cae64795b3b8022
Olga <nekrasova.kalina28@gmail.com>
2022-08-25 15:46:36 +0300

    Добавлен второй товар

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const item = "Носки фиолетовые";
-const quantity = 10;
-const category = "Носки";
-const price = 100;
+'use strict';
 
-console.log('Название товара:', item);
-console.log('Количество товара:', price * quantity, 'руб.' );
+{
+    const item = "Носки фиолетовые";
+    const quantity = 10;
+    const category = "Носки";
+    const price = 100;
+    
+    console.log('Название товара:', item);
+    console.log('Количество товара:', price * quantity, 'руб.' );
+}
+
+{
+    const item = "Кружка 300 мл, MILLIMI Милый дом, керамика";
+    const quantity = 100;
+    const category = "Кружки";
+    const price = 250;
+
+    console.log('Название товара:', item);
+    console.log('Количество товара:', price * quantity, 'руб.' );
+}
+
 
